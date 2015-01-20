19bc756e7cefd889423d56c77da99c4058b97e82
a1348563 <a1348563@unet.univie.ac.at>
2015-01-20 20:55:38 +0100

    bei Melden posts werden geloescht, post_serialisierung, postdai

---
+++ 50plus/src/Management/PinnwandDAO.java
@@ -0,0 +1,22 @@
+package Management;
+
+import java.util.ArrayList;
+
+import Personen.Post;
+
+public interface PinnwandDAO 
+{
+	public void speicherePost(Post p, int postNumber);
+	public ArrayList<Post> getOwnpostlist(String username);
+	public void loeschePost(int postNumber);
+    public ArrayList<Integer> getPostNumbers(String username);
+    public Post getPostbyNumber (int postNumber);
+    public void changeFlag(boolean flag, int postNumber);
+    public  ArrayList<Post> getFlaggedPostlist();
+    public void loescheAllPosts(String username);
+	
+	
+	
+	
+	
+}
